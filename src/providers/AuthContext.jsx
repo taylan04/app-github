0fea6221307e7894f2eb0c0fa5986c4391d01bdd
Taylan da Silva Gonzaga <tsgonzaga@cagen.local>
2026-06-12 16:51:59 -0300

    feat: adicionando reducer no contexto de autenticação

--- src/providers/AuthContext.jsx
+++ src/providers/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useReducer } from 'react';
 
 const AuthContext = createContext();
 
@@ -6,17 +6,29 @@ export function useAuth() {
     return useContext(AuthContext);
 }
 
+const valoresIniciais = {
+    user: null,
+    registeredUser: null,
+}
+
+function authReducer(valores, action) {
+    switch(action.type) {
+        case "set_user":
+            return {...valores, user: action.payload};
+        case "set_resgisteredUser":
+            return {...valores, registeredUser: action.payload};
+        default:
+            return valores;
+    }
+} 
+
 export default function AuthProvider({ children }) {
-    const [registeredUser, setRegisteredUser] = useState(null);
-    const [user, setUser] = useState(null);
+    
+    const [valores, dispatch] = useReducer(authReducer, valoresIniciais);
 
-    const register = (nome, telefone, email, password) => {
-        setRegisteredUser({ nome, telefone, email, password });
-    }
+    const register = (nome, telefone, email, password) => dispatch({type: "set_user", payload: {nome, telefone, email, password}});
 
-    const updateUser = (nome, telefone, email) => {
-        setUser({ nome, telefone, email });
-    }
+    const updateUser = (nome, telefone, email) => dispatch({type: "set_user", payload: {nome, telefone, email}});
 
     /*const login = (email, password) => {
         if (registeredUser && email === registeredUser.email && password === registeredUser.password) {
@@ -29,14 +41,15 @@ export default function AuthProvider({ children }) {
 
     const login = (email, password) => {
         if (email === "taylansilva0402@gmail.com" && password === "tay") {
-            setUser({ nome: "Taylan", email: email, telefone: "123456789" });
+            //setUser({ nome: "Taylan", email: email, telefone: "123456789" });
+            dispatch({type: "set_user", payload: { nome: "Taylan", email: "taylansilva0402@gmail.com", senha: "tay"}});
             return true;
         }
         return false;
     };
 
     const logout = () => {
-        setUser(null);
+        dispatch({type: "set_user", payload: null});
     };
 
     return (
